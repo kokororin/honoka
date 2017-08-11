812ff655721bd36b603f0daf0b19a8cfdd427679
kokororin <i@kotori.love>
2017-08-11 22:22:35 +0800

    Update tests

--- test/honokaTest.js
+++ test/honokaTest.js
@@ -2,24 +2,44 @@ import fetchMock from 'fetch-mock';
 import honoka from '../src/honoka';
 import pkg from '../package.json';
 
+const mockJsonResponse = (url = '*') => {
+  fetchMock.mock(url, {
+    body: { hello: 'world' },
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+};
+
 describe('honoka', () => {
   afterEach(() => {
     fetchMock.restore();
   });
 
-  it('honoka.version shoule return a version string', () => {
+  it('honoka.version should return a version string', () => {
     expect(honoka.version).to.equal(pkg.version);
   });
 
   it('honoka() should return a body when status is 200', async () => {
-    fetchMock.mock('*', { hello: 'world' });
+    mockJsonResponse();
     const response = await honoka('http://www.google.com');
     expect(response).to.deep.equal({ hello: 'world' });
   });
 
   it('honoka.get() should return a body when status is 200', async () => {
-    fetchMock.mock('*', { hello: 'world' });
+    mockJsonResponse();
     const response = await honoka('http://www.google.com');
+    expect(response).to.deep.equal({ hello: 'world' });
+  });
+
+  it('honoka() should build query strings correctly', async () => {
+    mockJsonResponse('http://www.google.com/?q=honoka&ie=UTF-8');
+    const response = await honoka('http://www.google.com/', {
+      data: {
+        q: 'honoka',
+        ie: 'UTF-8'
+      }
+    });
     expect(response).to.deep.equal({ hello: 'world' });
   });
 
